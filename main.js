8cd70c61640f45281bac906fa3e3fed93bc09d98
luise-go <luise.goede@yahoo.de>
2021-05-07 12:08:51 +0200

    add dom js

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 import './style.css';
+import { createElement } from './utils/elements';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+const header = createElement('header', {
+  className: 'hero',
+  children: [
+    createElement('h1', {
+      className: 'hero__title',
+      innerText: 'TRONALD DUMP',
+    }),
+  ],
+});
+const main = createElement('main', {
+  className: 'main',
+  children: [
+    createElement('input', {
+      className: 'input',
+      placeholder: 'Search for dumbest quotes...',
+      autofocus: true,
+    }),
+  ],
+});
+
+const footer = createElement('footer', {
+  className: 'footer',
+  children: [createElement('span', { innerText: 'Made with stupidity ' })],
+});
+
+document.querySelector('#app').append(header, main, footer);
